c84e02c388bda7821a9dba58190a336dd0972f30
SneakyCrago <crago@inbox.ru>
2017-04-03 23:28:58 +0300

    Corridor updated(topWall up + 24)

--- core/src/com/sneakycrago/undercore/objects/Corridor.java
+++ core/src/com/sneakycrago/undercore/objects/Corridor.java
@@ -31,8 +31,8 @@ public class Corridor {
         posBlock = new Vector2(0,11);
         velocity = new Vector2();
 
-        topLeftRects = new Rectangle[8];
-        topRightRects = new Rectangle[8];
+        topLeftRects = new Rectangle[7];
+        topRightRects = new Rectangle[7];
         bottomRets = new Rectangle[15];
 
         createRects(x);
@@ -52,12 +52,12 @@ public class Corridor {
         moveRects(x);
         endZone.setX(posBlock.x + x + BLOCK_SIZE);
 
-        checkScore();
+        //checkScore();
     }
     //SCORE
     boolean halfScore = false;
     boolean secondHalfScore = false;
-    public void checkScore() {
+    /*public void checkScore() {
         if(bottomRets[7].getX() <= 96+16 && !halfScore) {
             halfScore = true;
             Score.addGameScore(3);
@@ -66,7 +66,7 @@ public class Corridor {
             secondHalfScore = true;
             Score.addGameScore(3);
         }
-    }
+    } */
 
     //RECTS
     //create rectangles
@@ -94,11 +94,11 @@ public class Corridor {
         }
     }
     public void moveTopRightRects(float x) {
-        topRightRects[0].setX(posBlock.x + x + 32*8+ 32*3 -3);
-        for(int i =1; i<7; i++){
-            topRightRects[i].setX(posBlock.x + x +TEXTURE_SIZE*(8+3) + TEXTURE_SIZE*i - 3);
+        topRightRects[0].setX(posBlock.x +32+ x + 32*8+ 32*3 -3);
+        for(int i =1; i<6; i++){
+            topRightRects[i].setX(posBlock.x +32 + x +TEXTURE_SIZE*(8+3) + TEXTURE_SIZE*i - 3);
         }
-        topRightRects[7].setX(posBlock.x + x + BLOCK_SIZE-32 - 3);
+        topRightRects[6].setX(posBlock.x + x + BLOCK_SIZE-32 - 3);
     }
     public void moveBottomRects(float x) {
         bottomRets[0].setX(posBlock.x + x+ 32*2);
@@ -117,24 +117,24 @@ public class Corridor {
     //create elements
     public void createTopLeftRects(float x) {
         topLeftRects[0] = new Rectangle();
-        topLeftRects[0].set(posBlock.x + x, posBlock.y + FREE_SPACE, 32, TEXTURE_SIZE*6);
+        topLeftRects[0].set(posBlock.x + x, posBlock.y +24+ FREE_SPACE, 32, TEXTURE_SIZE*6 - 24);
         for(int i = 1; i < topLeftRects.length; i++) {
             topLeftRects[i] = new Rectangle();
-            topLeftRects[i].set(posBlock.x + x+ TEXTURE_SIZE*2, posBlock.y+ FREE_SPACE + HEIGHT*i,
-                    TEXTURE_SIZE, TEXTURE_SIZE*6 -HEIGHT*i);
+            topLeftRects[i].set(posBlock.x + x+ TEXTURE_SIZE*2, posBlock.y+ 24+FREE_SPACE + HEIGHT*i,
+                    TEXTURE_SIZE, TEXTURE_SIZE*6 -HEIGHT*i - 24);
         }
     }
     public void createTopRightRects(float x) {
         topRightRects[0] = new Rectangle();
-        topRightRects[0].set(posBlock.x + x + TEXTURE_SIZE*(8+3) -3, posBlock.y+ FREE_SPACE + HEIGHT*7, TEXTURE_SIZE, HEIGHT);
-        for(int i = 1; i < 7; i++) {
+        topRightRects[0].set(posBlock.x + x + TEXTURE_SIZE*(8+3) -3 + 32, posBlock.y+ FREE_SPACE + HEIGHT*7, TEXTURE_SIZE, HEIGHT);
+        for(int i = 1; i < 6; i++) {
             topRightRects[i] = new Rectangle();
-            topRightRects[i].set(posBlock.x + x +TEXTURE_SIZE*(8+3) + TEXTURE_SIZE*i - 3, posBlock.y + FREE_SPACE + + HEIGHT*7-HEIGHT*i,
-                    TEXTURE_SIZE, HEIGHT +HEIGHT*i);
+            topRightRects[i].set(posBlock.x +32 + x +TEXTURE_SIZE*(8+3) + TEXTURE_SIZE*i - 3,
+                    posBlock.y + FREE_SPACE + + HEIGHT*7-HEIGHT*i, TEXTURE_SIZE, HEIGHT +HEIGHT*i);
         }
-        topRightRects[7] = new Rectangle();
-        topRightRects[7].set(posBlock.x + x + BLOCK_SIZE-TEXTURE_SIZE - 3,posBlock.y+ FREE_SPACE,
-                TEXTURE_SIZE + 3, TEXTURE_SIZE*6);
+        topRightRects[6] = new Rectangle();
+        topRightRects[6].set(posBlock.x + x + BLOCK_SIZE-TEXTURE_SIZE - 3,posBlock.y+ FREE_SPACE + 24,
+                TEXTURE_SIZE + 3, TEXTURE_SIZE*6 -24);
     }
     public void createBottomRects(float x) {
         bottomRets[0] = new Rectangle();
@@ -192,35 +192,35 @@ public class Corridor {
     }
     public void drawTopLeft(ShapeRenderer shapeRenderer, float x) {
         shapeRenderer.setColor(Color.BLACK);
-        shapeRenderer.rect(posBlock.x + x+ 3, 310 -11, (TEXTURE_SIZE * 9)-6, 3);
+        shapeRenderer.rect(posBlock.x + x+ 3, 310 -11, (TEXTURE_SIZE * 7)-6, 3);
 
         shapeRenderer.setColor(22/255f,238/255f,247/255f,1f);
 
-        shapeRenderer.rect(posBlock.x + x,posBlock.y +FREE_SPACE,3, TEXTURE_SIZE*6);
+        shapeRenderer.rect(posBlock.x + x,posBlock.y +FREE_SPACE + 24,3, TEXTURE_SIZE*5 + 8);
 
-        shapeRenderer.rect(posBlock.x + x,posBlock.y +FREE_SPACE,TEXTURE_SIZE, 3);
-        for(int i = 1; i <9;i++) {
-            shapeRenderer.rect(posBlock.x + x +(TEXTURE_SIZE* i) -3, posBlock.y - 24+ FREE_SPACE+ HEIGHT * i,
+        shapeRenderer.rect(posBlock.x + x,posBlock.y +FREE_SPACE +24,TEXTURE_SIZE, 3);
+        for(int i = 1; i <8;i++) {
+            shapeRenderer.rect(posBlock.x + x +(TEXTURE_SIZE* i) -3, posBlock.y - 24+ FREE_SPACE + 24+ HEIGHT * i,
                     3, 24);
-            shapeRenderer.rect(posBlock.x + x +((TEXTURE_SIZE) * i) -3, posBlock.y- 24 + FREE_SPACE+ 24+ HEIGHT * i,
+            shapeRenderer.rect(posBlock.x + x +((TEXTURE_SIZE) * i) -3, posBlock.y- 24 + FREE_SPACE + 24+ 24+ HEIGHT * i,
                     TEXTURE_SIZE, 3);
         }
     }
     public void drawTopRight(ShapeRenderer shapeRenderer, float x){
         shapeRenderer.setColor(Color.BLACK);
-        shapeRenderer.rect(posBlock.x + x+ TEXTURE_SIZE * (8+3), 310-11, TEXTURE_SIZE*8 - 3,3);
+        shapeRenderer.rect(posBlock.x + x+ TEXTURE_SIZE * (8+3) + 32, 310-11, TEXTURE_SIZE*7 - 3,3);
 
         shapeRenderer.setColor(22/255f,238/255f,247/255f,1f);
-        shapeRenderer.rect(posBlock.x + x + TEXTURE_SIZE *19 -3,posBlock.y + FREE_SPACE,3, TEXTURE_SIZE*6);
+        shapeRenderer.rect(posBlock.x + x + TEXTURE_SIZE *19 -3,posBlock.y + FREE_SPACE + 24,3, TEXTURE_SIZE*5 + 8);
 
-        shapeRenderer.rect(posBlock.x + x + TEXTURE_SIZE* 18 -3, posBlock.y+ FREE_SPACE ,3,24);
-        shapeRenderer.rect(posBlock.x + x+ TEXTURE_SIZE*18 -3, posBlock.y +FREE_SPACE, TEXTURE_SIZE + 3,3);
+        shapeRenderer.rect(posBlock.x + x + TEXTURE_SIZE* 18 -3, posBlock.y+ FREE_SPACE +24,3,24);
+        shapeRenderer.rect(posBlock.x + x+ TEXTURE_SIZE*18 -3, posBlock.y +FREE_SPACE + 24, TEXTURE_SIZE + 3,3);
         //down
-        for(int i=7; i > 0;i--){
-            shapeRenderer.rect(posBlock.x + x + TEXTURE_SIZE* (7+3) +(TEXTURE_SIZE* i) -3, posBlock.y+ FREE_SPACE +HEIGHT*8 - HEIGHT * i,
-                    3, 24);
-            shapeRenderer.rect(posBlock.x + x +TEXTURE_SIZE* (7+3)+((TEXTURE_SIZE) * i) -3, posBlock.y+ FREE_SPACE + HEIGHT*8 - HEIGHT * i,
-                    TEXTURE_SIZE + 3, 3);
+        for(int i=7; i > 1;i--){
+            shapeRenderer.rect(posBlock.x + x + TEXTURE_SIZE* (7+3) +(TEXTURE_SIZE* i) -3,
+                    posBlock.y+ FREE_SPACE + 24 +HEIGHT*8 - HEIGHT * i, 3, 24);
+            shapeRenderer.rect(posBlock.x + x +TEXTURE_SIZE* (7+3)+((TEXTURE_SIZE) * i) -3,
+                    posBlock.y+ FREE_SPACE + 24  + HEIGHT*8 - HEIGHT * i, TEXTURE_SIZE + 3, 3);
         }
     }
 
